d707cba3f420847d921efc89f33f23d4222ae24d
root <root@LAPTOP-A2EG90UI.localdomain>
2020-10-31 09:03:53 +0800

    update

--- models.py
+++ models.py
@@ -2,9 +2,9 @@
 
 import tensorflow as tf;
 
-def AtrousSpatialPyramidPooling(input_shape):
+def AtrousSpatialPyramidPooling(channel):
 
-  inputs = tf.keras.Input(input_shape[-3:]);
+  inputs = tf.keras.Input((None, None, channel));
   # global pooling
   # results.shape = (batch, 1, 1, channel)
   results = tf.keras.layers.Lambda(lambda x: tf.math.reduce_mean(x, [1,2], keepdims = True))(inputs);
@@ -13,7 +13,7 @@ def AtrousSpatialPyramidPooling(input_shape):
   results = tf.keras.layers.BatchNormalization()(results);
   results = tf.keras.layers.ReLU()(results);
   # pool.shape = (batch, height, width, 256)
-  pool = tf.keras.layers.UpSampling2D(size = (input_shape[-3] // results.shape[1], input_shape[-2] // results.shape[2]), interpolation = 'bilinear')(results);
+  pool = tf.keras.layers.Lambda(lambda x: tf.image.resize(x[0], (tf.shape(x[1])[1], tf.shape(x[1])[2]), method = tf.image.ResizeMethod.BILINEAR))([results, inputs]);
   # results.shape = (batch, height, width, 256)
   results = tf.keras.layers.Conv2D(256, kernel_size = (1,1), dilation_rate = 1, padding = 'same', kernel_initializer = tf.keras.initializers.he_normal(), use_bias = False)(inputs);
   results = tf.keras.layers.BatchNormalization()(results);
@@ -43,14 +43,14 @@ def ResNet50(input_shape):
   resnet50 = tf.keras.applications.ResNet50(input_tensor = inputs, weights = 'imagenet', include_top = False);
   return tf.keras.Model(inputs = inputs, outputs = (resnet50.get_layer('conv4_block6_2_relu').output, resnet50.get_layer('conv2_block3_2_relu').output), name = 'resnet50');
 
-def DeeplabV3Plus(input_shape, nclasses = None):
+def DeeplabV3Plus(channel = 3, nclasses = None):
 
   assert type(nclasses) is int;
-  inputs = tf.keras.Input(input_shape[-3:]);
+  inputs = tf.keras.Input((None, None, 3));
   low, high = ResNet50(inputs.shape[1:])(inputs);
   # a.shape = (batch, height // 4, width // 4, 256)
-  results = AtrousSpatialPyramidPooling(low.shape[-3:])(low);
-  a = tf.keras.layers.UpSampling2D(size = (input_shape[-3] // 4 // results.shape[1], input_shape[-2] // 4 // results.shape[2]), interpolation = 'bilinear')(results);
+  results = AtrousSpatialPyramidPooling(low.shape[-1])(low);
+  a = tf.keras.layers.Lambda(lambda x: tf.image.resize(x[0], (tf.shape(x[1])[1] // 4, tf.shape(x[1])[2] // 4), method = tf.image.ResizeMethod.BILINEAR))([results, inputs]);
   # b.shape = (batch, height // 4, width // 4, 48)
   results = tf.keras.layers.Conv2D(48, kernel_size = (1,1), padding = 'same', kernel_initializer = tf.keras.initializers.he_normal(), use_bias = False)(high);
   results = tf.keras.layers.BatchNormalization()(results);
@@ -64,14 +64,14 @@ def DeeplabV3Plus(input_shape, nclasses = None):
   results = tf.keras.layers.Conv2D(256, kernel_size = (3,3), padding = 'same', activation = 'relu', kernel_initializer = tf.keras.initializers.he_normal(), use_bias = False)(results);
   results = tf.keras.layers.BatchNormalization()(results);
   results = tf.keras.layers.ReLU()(results);
-  results = tf.keras.layers.UpSampling2D(size = (input_shape[-3] // results.shape[1], input_shape[-2] // results.shape[2]), interpolation = 'bilinear')(results);
+  results = tf.keras.layers.Lambda(lambda x: tf.image.resize(x[0], (tf.shape(x[1])[1], tf.shape(x[1])[2]), method = tf.image.ResizeMethod.BILINEAR))([results, inputs]);
   results = tf.keras.layers.Conv2D(nclasses, kernel_size = (1,1), padding = 'same')(results);
   return tf.keras.Model(inputs = inputs, outputs = results);
 
 if __name__ == "__main__":
 
   assert True == tf.executing_eagerly();
-  deeplabv3 = DeeplabV3Plus((8,224,224,3),66);
+  deeplabv3 = DeeplabV3Plus(3,66);
   import numpy as np;
   results = deeplabv3(tf.constant(np.random.normal(size = (8, 224, 224, 3)), dtype = tf.float32));
   deeplabv3.save('deeplabv3.h5');

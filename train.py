1ed78a3f5100f0b737e4ae69878f8573ab1fe7ff
root <root@LAPTOP-A2EG90UI.localdomain>
2020-10-31 09:23:33 +0800

    update

---
+++ train.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import tensorflow as tf;
+from create_dataset import parse_function();
+
+def main():
+
+  
+
+if __name__ == "__main__":
+
+  assert tf.executing_eagerly();
+  main();

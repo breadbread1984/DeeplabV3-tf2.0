29a4fdfd4cb7b2f5cc6389da0b7f1602f8dbe4a4
breadbread1984 <breadbread1984@163.com>
2020-11-26 10:55:46 +0800

    update

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from os.path import exists, join;
 import numpy as np;
 import cv2;
 import tensorflow as tf;
+from models import DeeplabV3Plus;
 
 def main(img_path):
 
@@ -16,7 +17,11 @@ def main(img_path):
     print('no pretrained model found!');
     exit(1);
   data = tf.expand_dims(img.astype('float32') / 255., axis = 0); # data.shape = (1, h, w, 3)
-  deeplabv3plus = tf.keras.models.load_model(join('models', 'deeplabv3plus.h5'), custom_objects = {'tf': tf});
+  if exists(join('models', 'deeplabv3plus_weights.h5')):
+    deeplabv3plus = DeeplabV3Plus(1 + 80);
+    deeplabv3plus.load_weights(join('models', 'deeplabv3plus_weights.h5'));
+  else:
+    deeplabv3plus = tf.keras.models.load_model(join('models', 'deeplabv3plus.h5'), custom_objects = {'tf': tf});
   preds = deeplabv3plus(data); # preds.shape = (1, h, w, 1 + 80)
   seg = tf.argmax(preds[0:1,...], axis = -1); # cls.shape = (1, 256, 256)
   classes, _ = tf.unique(tf.reshape(seg, (-1,))); # cls.shape = (class num)
